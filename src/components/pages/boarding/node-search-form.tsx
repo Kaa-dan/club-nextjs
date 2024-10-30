c8054a9b66fb4be8ba21c44573bdf19c33320901
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-10-30 15:27:00 +0530

    ADD node card join ui and search

--- src/components/pages/boarding/node-search-form.tsx
+++ src/components/pages/boarding/node-search-form.tsx
@@ -10,28 +10,50 @@ import { NODES } from "@/lib/constants/nodes";
 import NodeCardMini from "@/components/globals/node/node-card";
 import AddNodeDialog from "./node/add-node-dialog";
 import { getNodes } from "./endpoint";
+import NodeJoinCard from "./node/node-join-card";
 
 interface ISearchResultsProps {
   setShowAddNodeDialog: (bool: boolean) => void;
+  searchTerm: string;
+  setSearchTerm: (term: string) => void;
 }
-const SearchResults = ({ setShowAddNodeDialog }: ISearchResultsProps) => {
+const SearchResults = ({
+  setShowAddNodeDialog,
+  searchTerm,
+  setSearchTerm,
+}: ISearchResultsProps) => {
   const [nodes, setNodes] = useState([]);
+  const [filteredNodes, setFilteredNodes] = useState([]);
   useEffect(() => {
     getNodes().then((res) => {
       setNodes(res);
     });
   }, []);
+  useEffect(() => {
+    setFilteredNodes(
+      searchTerm
+        ? nodes.filter((node: any) =>
+            node.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+        : nodes
+    );
+  }, [searchTerm]);
   return (
     <div className="flex flex-col gap-2 px-8">
       <h2 className="text-lg font-semibold">Search node</h2>
       <div className="flex items-center gap-2 rounded-sm bg-slate-100">
         <Input
+          onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
           placeholder="Enter name"
           className="h-8 w-full border-none bg-slate-100"
         />
-        <X className="text-slate-600" />
+        <X
+          onClick={() => setSearchTerm("")}
+          className="text-slate-600 cursor-pointer"
+        />
       </div>
-      <div className="mt-4 flex flex-wrap gap-5 overflow-y-scroll h-52 ">
+      <div className="mt-4 flex flex-wrap gap-5 justify-center items-center overflow-y-scroll h-52 ">
         <div
           className="flex size-36 cursor-pointer flex-col items-center justify-center gap-1 rounded-sm border-2 border-dashed border-primary p-3 text-base text-primary"
           onClick={() => setShowAddNodeDialog(true)}
@@ -39,8 +61,15 @@ const SearchResults = ({ setShowAddNodeDialog }: ISearchResultsProps) => {
           <Plus />
           <span>Create Node</span>
         </div>
-        {nodes.map((node: any, index) => {
-          return <NodeCardMini key={node.name} node={node} />;
+        {filteredNodes.map((node: any, index) => {
+          return (
+            <NodeJoinCard
+              onJoin={() => {}}
+              requested={false}
+              key={node.name}
+              node={node}
+            />
+          );
         })}
       </div>
     </div>
@@ -51,16 +80,26 @@ export const NodeSearchForm = () => {
   const [tncAccepted, setTncAccepted] = useState(false);
   const [showResults, setShowResults] = useState(false);
   const [showAddNodeDialog, setShowAddNodeDialog] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="mb-6 flex w-full flex-col">
       <AddNodeDialog open={showAddNodeDialog} setOpen={setShowAddNodeDialog} />
       {showResults ? (
-        <SearchResults setShowAddNodeDialog={setShowAddNodeDialog} />
+        <SearchResults
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          setShowAddNodeDialog={setShowAddNodeDialog}
+        />
       ) : (
         <>
           <div className="mx-auto flex w-3/5 flex-col gap-2">
             <Label>Enter your node name</Label>
-            <Input className="mb-4" placeholder="Enter name" />
+            <Input
+              onChange={(e) => setSearchTerm(e.target.value)}
+              value={searchTerm}
+              className="mb-4"
+              placeholder="Enter name"
+            />
             <Label>Pin code</Label>
             <Input placeholder="Enter code" />
             <Button
--- src/components/pages/boarding/node/node-join-card.tsx
+++ src/components/pages/boarding/node/node-join-card.tsx
@@ -1,43 +1,76 @@
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { NodeData } from "@/types";
 import { MapPin, Users } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
-interface INode {
-  name: string;
-  image: string;
-  memberCount: number;
-  location: string;
-  country: string;
-}
-
 const NodeJoinCard: React.FC<{
-  node: INode;
+  node: NodeData;
   requested: boolean;
   onJoin: () => void;
 }> = ({
-  node: { country, name, image, memberCount, location },
+  node: { name, profileImage, members, coverImage, location },
   requested,
   onJoin,
 }) => {
   return (
-    <Card className="flex flex-col text-xs rounded-sm gap-1 size-[9rem] p-3">
+    <Card className="flex flex-col text-xs rounded-sm gap-1 w-[9rem] p-3">
       <Image
         height={200}
         width={200}
-        src={image}
+        src={profileImage}
         alt={name}
         className="size-10 object-cover rounded-md"
       />
       <span className="text-sm font-semibold w-full truncate">{name}</span>
       <div className="flex items-center gap-1">
         <Users size={"1rem"} />
-        {memberCount}
+        {members.length}
       </div>
       <div className="w-4/5 p-1 bg-slate-50 rounded-md flex items-center">
         <MapPin size={"1rem"} className="text-red-500" />
-        {`${location}, ${country}`}
+        <span className="truncate">{`${location}`}</span>
       </div>
+      <Dialog>
+        <DialogTrigger className="w-full">
+          <Button className="p-0 h-6 w-full">Join</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogTitle>Join {name}</DialogTitle>
+          {/*cover photo and profile photo : profile photo half should cover bottom of cover photo*/}
+          <div className="relative mb-8">
+            <Image
+              height={200}
+              width={400}
+              src={coverImage}
+              alt={name}
+              className="h-32 w-96 mx-auto"
+            />
+            <Image
+              height={200}
+              width={200}
+              src={profileImage}
+              alt={name}
+              className="size-20 object-cover absolute rounded-full border-4 border-white -bottom-10 left-1/2 -translate-x-1/2"
+            />
+          </div>
+
+          <span className="text-center text-base text-slate-500">
+            Are you sure you want to join this node?
+          </span>
+          <Button className="w-1/3 mx-auto" onClick={onJoin}>
+            Confirm
+          </Button>
+        </DialogContent>
+      </Dialog>
     </Card>
   );
 };
