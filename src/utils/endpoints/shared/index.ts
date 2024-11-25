ffdff4e8b41d149ca12a63c54135c0180b1a6d63
ashmil19 <ashmilabdulrasheed@gmail.com>
2024-11-25 18:47:35 +0530

    changes in

--- src/utils/endpoints/shared/index.ts
+++ src/utils/endpoints/shared/index.ts
@@ -2,7 +2,7 @@ import { withTokenAxios } from "@/lib/mainAxios";
 
 export class SharedEndpoints {
   static async search(term: string, tag?: string) {
-    const response = await mainAxios.get("/search", {
+    const response = await withTokenAxios.get("/search", {
       params: {
         term,
         tag,
@@ -12,23 +12,23 @@ export class SharedEndpoints {
   }
 
   static async makeAdmin(data: any) {
-    const response = await mainAxios.put("/users/make-admin", data);
+    const response = await withTokenAxios.put("/users/make-admin", data);
     return response.data;
   }
 
   static async makeModerator(data: any) {
-    const response = await mainAxios.put("/users/make-moderator", data);
+    const response = await withTokenAxios.put("/users/make-moderator", data);
     return response.data;
   }
 
   static async makeMember(data: any) {
     console.log("first");
-    const response = await mainAxios.put("/users/make-member", data);
+    const response = await withTokenAxios.put("/users/make-member", data);
     return response.data;
   }
 
   static async removeMember(data: any) {
-    const response = await mainAxios.put("/users/remove-member", data);
+    const response = await withTokenAxios.put("/users/remove-member", data);
     return response.data;
   }
 }
