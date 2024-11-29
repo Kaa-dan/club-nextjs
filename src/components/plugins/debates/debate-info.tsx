2020395557bda07c236753ed12785f10b9e6a8af
Rishal-KV <rishkv923@gmail.com>
2024-11-29 15:19:24 +0530

    bug fix

--- src/components/plugins/debates/debate-info.tsx
+++ src/components/plugins/debates/debate-info.tsx
@@ -31,11 +31,13 @@ type AdoptionOption = {
     clubId: string;
     role: string;
     name: string;
+    image: string;
   }[];
   nonAdoptedNodes: {
     nodeId: string;
     role: string;
     name: string;
+    image: string;
   }[];
 };
 
