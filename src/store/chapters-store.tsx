839e735da884aa7b710d497e20f56dae7ba48369
ashmil19 <ashmilabdulrasheed@gmail.com>
2024-12-31 18:48:24 +0530

    chapter updated

--- src/store/chapters-store.tsx
+++ src/store/chapters-store.tsx
@@ -3,10 +3,14 @@ import { create } from "zustand";
 
 interface ChapterState {
   publishedChapters: TChapter[];
+  proposedChapters: TChapter[];
   setPublishedChapters: (chapters: TChapter[]) => void;
+  setProposedChapters: (chapters: TChapter[]) => void;
 }
 
 export const useChapterStore = create<ChapterState>()((set) => ({
   publishedChapters: [],
+  proposedChapters: [],
   setPublishedChapters: (publishedChapters) => set({ publishedChapters }),
+  setProposedChapters: (proposedChapters) => set({ proposedChapters }),
 }));
