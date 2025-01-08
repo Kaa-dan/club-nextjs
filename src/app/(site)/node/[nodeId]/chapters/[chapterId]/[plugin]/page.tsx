c4b0f3f7b1d23d06884208ae0d6da495df5a6bc3
Muhsin-42 <muhsinofficial21@gmail.com>
2025-01-08 16:55:51 +0530

    Working on Chapter Modules

---
+++ src/app/(site)/node/[nodeId]/chapters/[chapterId]/[plugin]/page.tsx
@@ -0,0 +1,43 @@
+import RulesLayout from "@/components/plugins/rules-regulations/rules-layout";
+import IssuesLayout from "@/components/plugins/issues/issues-layout";
+import React from "react";
+import DebateLayout from "@/components/plugins/debates/debate-layout";
+import ProjectLayout from "@/components/plugins/projects/project-layout";
+const PluginPage = async ({
+  params,
+}: {
+  params: Promise<{ plugin: string; clubId: string }>;
+}) => {
+  const { plugin, clubId } = await params;
+
+  const renderPluginContent = () => {
+    switch (plugin) {
+      case "issues":
+        return (
+          <IssuesLayout forum={"chapter"} forumId={clubId} plugin={plugin} />
+        );
+      case "rules":
+        return (
+          <RulesLayout forum={"chapter"} forumId={clubId} plugin={plugin} />
+        );
+      case "debate":
+        return (
+          <DebateLayout forumId={clubId} forum="chapter" plugin={plugin} />
+        );
+      case "projects":
+        return (
+          <ProjectLayout forum="chapter" forumId={clubId} plugin={plugin} />
+        );
+      default:
+        return (
+          <div className="flex h-full items-center justify-center">
+            <span className="text-muted-foreground">Plugin not found</span>
+          </div>
+        );
+    }
+  };
+
+  return <main>{renderPluginContent()}</main>;
+};
+
+export default PluginPage;
